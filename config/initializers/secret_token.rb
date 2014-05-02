00f443d1275bb4b0d59aa7d750e9cb149ebc8eeb
root <alevellop@gmail.com>
2014-05-02 18:37:46 +0200

    First commit, installed gem 'mongoid' and 'bson_ext'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monrails::Application.config.secret_key_base = '593ddd09adccdeb39de990c328e50126cc2cd517f5168e2009dd664a57e1fb84cb4a08a32823240fc90f72d15e55df6f432e0a34b89c063790fa21d543f925dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Monrails::Application.config.session_store :cookie_store, key: '_monrails_session'
