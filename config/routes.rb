6b3158fec0ab7fc6cdeed95bb2710112f13acf06
root <alevellop@gmail.com>
2014-05-10 14:57:29 +0200

    Finish document User and simple Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Monrails::Application.routes.draw do
+  
   root "static_pages#home"
+
+  get "users/new"
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
