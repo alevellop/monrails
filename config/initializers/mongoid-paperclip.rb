762cd703dcbe39225b8ef8b5573fdd0144d3dbbd
root <alevellop@gmail.com>
2014-09-03 07:33:17 +0200

    Refactor mongoid-paperclip.rb to fix heroku load image

--- config/initializers/mongoid-paperclip.rb
+++ config/initializers/mongoid-paperclip.rb
@@ -2,7 +2,7 @@ require 'mongoid_paperclip'
 
 Paperclip::Attachment.default_options[:storage] = :s3
 # Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:s3_protocol] = 'https'
+# Paperclip::Attachment.default_options[:s3_protocol] = 'https'
 Paperclip::Attachment.default_options[:s3_credentials] =
   { bucket: Figaro.env.s3_bucket_name,
   	access_key_id: Figaro.env.aws_access_key_id,
